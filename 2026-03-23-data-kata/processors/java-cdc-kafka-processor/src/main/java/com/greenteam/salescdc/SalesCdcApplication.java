fd42b4e83896f85dc8120ab88d4abcf25b48e38f
João Guilherme de Sá <jaum_ipm@hotmail.com>
2026-03-05 22:18:48 -0300

    cdc processor

---
+++ 2026-03-23-data-kata/processors/java-cdc-kafka-processor/src/main/java/com/greenteam/salescdc/SalesCdcApplication.java
@@ -0,0 +1,12 @@
+package com.greenteam.salescdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesCdcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesCdcApplication.class, args);
+    }
+}
