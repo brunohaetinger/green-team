779236a76438e4f100fda992c5d127c3430e96f6
Anderson Babinski <andeerlb@gmail.com>
2026-03-12 02:22:16 -0300

    ajustado conectores

--- 2026-03-23-data-kata/datasources/internal/report-api/init.sql
+++ 2026-03-23-data-kata/datasources/internal/report-api/init.sql
@@ -1,6 +1,13 @@
-CREATE TABLE sales (
-    id serial PRIMARY KEY,
-    salesman_id INT NOT NULL,
-    store_id INT NOT NULL,
-    total INT
+CREATE TABLE IF NOT EXISTS sales (
+    id SERIAL PRIMARY KEY,
+    salesman_id INTEGER NOT NULL,
+    product_id INTEGER NOT NULL,
+    quantity INTEGER NOT NULL
 );
+
+INSERT INTO sales (salesman_id, product_id, quantity)
+SELECT 
+    floor(random() * 20 + 1)::int as salesman_id,
+    floor(random() * 201 + 100)::int as product_id,
+    floor(random() * 100 + 1)::int as quantity
+FROM generate_series(1, 500);
