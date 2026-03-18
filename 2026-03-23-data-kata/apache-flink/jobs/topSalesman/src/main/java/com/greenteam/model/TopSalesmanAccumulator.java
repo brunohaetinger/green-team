f0f2b94f8d3a9555f007d744166d3a4939b8fab5
emersonpisoni <emersonpisoni14@gmail.com>
2026-03-18 11:29:09 -0300

    create more fields

--- 2026-03-23-data-kata/apache-flink/jobs/topSalesman/src/main/java/com/greenteam/model/TopSalesmanAccumulator.java
+++ 2026-03-23-data-kata/apache-flink/jobs/topSalesman/src/main/java/com/greenteam/model/TopSalesmanAccumulator.java
@@ -1,11 +1,14 @@
 package com.greenteam.model;
 
 import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDate;
 
 public class TopSalesmanAccumulator {
     public int salesmanId = -1;
     public String salesmanName;
-    public String saleDate;
+    public LocalDate saleDate;
     public BigDecimal totalAmount = BigDecimal.ZERO;
     public long totalUnits = 0;
+    public Instant processedAt;
 }
