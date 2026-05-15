8616ef333e3fec8844bb18533f26b1713355f375
emersonpisoni <emersonpisoni14@gmail.com>
2026-05-15 10:54:02 -0300

    resolve comments

--- 2026-06-22-tree-ds-kata/emersons-solution/src/FenwickTree.kt
+++ 2026-06-22-tree-ds-kata/emersons-solution/src/FenwickTree.kt
@@ -3,12 +3,26 @@ class FenwickTree(size: Int) {
     private val tree = LongArray(size + 1)
     val size: Int get() = tree.size - 1
 
+    constructor(values: LongArray) : this(values.size) {
+        for (i in values.indices) {
+            tree[i + 1] = values[i]
+        }
+        for (i in 1..size) {
+            val parent = i + lowestSetBit(i)
+            if (parent <= size) {
+                tree[parent] += tree[i]
+            }
+        }
+    }
+
+    private fun lowestSetBit(i: Int): Int = i and -i
+
     fun update(index: Int, delta: Long) {
         require(index in 1..size) { "index $index out of range [1..$size]" }
         var i = index
         while (i <= size) {
             tree[i] += delta
-            i += i and -i
+            i += lowestSetBit(i)
         }
     }
 
@@ -18,10 +32,11 @@ class FenwickTree(size: Int) {
         var sum = 0L
         while (i > 0) {
             sum += tree[i]
-            i -= i and -i
+            i -= lowestSetBit(i)
         }
         return sum
     }
+
 
     fun rangeSum(left: Int, right: Int): Long {
         require(left in 1..right && right <= size)
